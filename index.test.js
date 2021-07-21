75d04f8914a23c96e2de787191b2fb767becb622
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-21 12:45:25 -0700

    4 tests passing

--- index.test.js
+++ index.test.js
@@ -24,7 +24,11 @@ describe('[Exercise 2] trimPropertiesMutation', () => {
     expect(actual).toEqual(expected)
    })
   
-  // test('[4] the object returned is the exact same one we passed in', () => {})
+   test('[4] the object returned is the exact same one we passed in', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const actual = utils.trimPropertiesMutation(input)
+    expect(actual).toBe(input)
+   })
 })
 
 describe('[Exercise 3] findLargestInteger', () => {
